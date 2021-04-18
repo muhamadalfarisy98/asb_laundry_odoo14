e9aac35f33ef0f786ce977af74d8551d22ecdfb2
muhamadalfarisy98 <muhamadalfarisy9d@yahoo.com>
2021-04-19 00:10:42 +0700

    [ADD] menu laundry

--- laundry/__manifest__.py
+++ laundry/__manifest__.py
@@ -10,7 +10,7 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "Muhamad Alfarisy",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
@@ -20,16 +20,29 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': [
+        'base',
+        'mail',
+        'uom',
+        'contacts',
+        'sale',
+        'product',
+        'account',
+    ],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+        # 'views/views.xml',
+        'views/menu.xml',
+
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
